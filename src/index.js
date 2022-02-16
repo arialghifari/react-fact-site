2c3cde8735616e509565391ba02868e3e40c1c23
arialghifari <arialghifari99@gmail.com>
2022-02-16 16:09:11 +0800

    Add navbar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import "./main.css";
 
-const App = () => {
+const Navbar = () => {
     return (
-        <div className="App">
-            <p>Hello World!</p>
-        </div>
+        <nav className="navbar h-[100px] flex px-10 justify-between items-center bg-[#21222A] text-white">
+            <div className="logo">
+                <a href="/" className="text-[#61DAFB] font-bold text-[32px]">
+                    ReactFacts
+                </a>
+            </div>
+            <div className="project-1">
+                <p className="text-lg font-semibold">
+                    React Course - Project 1
+                </p>
+            </div>
+        </nav>
     );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const MainContent = () => {
+    return (
+        <main className="main-content p-10">
+            <h1 className="text-lg">React is awesome!</h1>
+        </main>
+    );
+};
+
+ReactDOM.render(
+    <div className="font-inter">
+        <Navbar />
+        <MainContent />
+    </div>,
+    document.getElementById("root")
+);
