f86a0a1a1584eaba8ccf4b63ed57f6194559d346
arialghifari <arialghifari99@gmail.com>
2022-02-16 11:52:16 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./main.css";
+
+const App = () => {
+    return (
+        <div className="App">
+            <p>Hello World!</p>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
